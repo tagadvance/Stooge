0a6706a156b05a40fd698dd659327c9943adf0da
Tag <tagspilman@gmail.com>
2017-06-13 22:13:16 -0700

    add return type

--- src/tagadvance/stooge/CurlException.php
+++ src/tagadvance/stooge/CurlException.php
@@ -10,16 +10,21 @@ namespace tagadvance\stooge;
 class CurlException extends \Exception {
 	
 	/**
-	 * 
-	 * @param string $message
-	 * @param int $code One of the <a href="https://curl.haxx.se/libcurl/c/libcurl-errors.html">cURL error code</a> constants.
-	 * @param Exception $previous
+	 *
+	 * @param string $message        	
+	 * @param int $code
+	 *        	One of the <a href="https://curl.haxx.se/libcurl/c/libcurl-errors.html">cURL error code</a> constants.
+	 * @param Exception $previous        	
 	 */
 	function __construct($message, $code, Exception $previous = null) {
 		parent::__construct ( $message, $code, $previous );
 	}
 	
-	function getErrorMessage() {
+	/**
+	 * 
+	 * @return string
+	 */
+	function getErrorMessage(): string {
 		$code = $this->getCode ();
 		return curl_strerror ( $code );
 	}
